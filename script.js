1f353cf0d5fdf979e3dacb048aed805022734bb6
Gallardo Lee <leegallardo16@gmail.com>
2024-02-07 14:32:33 +0800

    Improve game flow logic

--- script.js
+++ script.js
@@ -31,10 +31,15 @@ const gameBoard = (function() {
         console.log(gameBoard.board);
     };
 
+    const resetGameBoard = function() {
+        gameBoard = {board: []};
+    };
+
     const getGameBoard = () => gameBoard;
 
     pubsub.on("createGameBoard", createGameBoard);
     pubsub.on("editGameBoard", editGameBoard);
+    pubsub.on("resetGameBoard", resetGameBoard);
 
     return {getGameBoard};
 })();
@@ -45,91 +50,107 @@ function createPlayer (marker, title) {
     return {name, marker, title};
 };
 
-//module for game flow
-const ticTacToeGame = (function() {
-    let firstPlayer = createPlayer("X", "Player 1");
-    let secondPlayer = createPlayer("O", "Player 2");
-    let players = [firstPlayer, secondPlayer];
-    let activePlayer = players[0];
-    let valid = false;
+//gameOver module
+const gameOver = (function() {
+    dialog = document.querySelector("#play-again-dialog");
+    winner = document.querySelector("#winner");
+    confirmButton = document.querySelector("#ok-button");
 
-    pubsub.emit("createGameBoard", 3, 3);
+    const openGameOver = function(activePlayer) {
+        winner.textContent = `${activePlayer} is the winner!`
+        dialog.showModal();
+    };
 
-    const initRound = function() {
-        document.body.addEventListener("mousedown", (e) => {
-            if (e.target.classList.contains("cell")) {
-                enableCheckWin();
-                if (!checkWin(gameBoard.getGameBoard())) {
-                    enablePlaceMarker();
-                    enableSwitchPlayer();
-                } else {
-                    disablePlaceMarker();
-                    disableSwitchPlayer();
-                };
-                console.log(checkWin(gameBoard.getGameBoard()));
-            };
+    const confirmRestart = function() {
+        confirmButton.addEventListener("click", (e) => {
+            pubsub.emit("restartGame");
         });
     };
 
-    const placeMarker = (e) => {
-        if (e.target.classList.contains("cell")) {
-            if (e.target.textContent == "") {
-                e.target.textContent = activePlayer.marker;
-                currentRow = Number(e.target.classList[1].at(-1)) - 1;
-                currentColumn = Number(e.target.classList[2].at(-1)) - 1;
-                pubsub.emit("editGameBoard", activePlayer, currentRow, currentColumn);
-                valid = true;
-            } else {
-                alert("This spot is taken!");
-                valid = false;
+    pubsub.on("openGameOver", openGameOver)
+    confirmRestart();
+})();
+
+const ticTacToeGame = (function() {
+
+    const startGame = function() {
+        controlGame();
+    };
+    
+    const controlGame = function() {
+        pubsub.emit("createGameBoard", 3, 3)
+        let firstPlayer = createPlayer("X", "Player 1");
+        let secondPlayer = createPlayer("O", "Player 2");
+        players = [firstPlayer, secondPlayer];
+        activePlayer = players[0];
+
+        const placeMarker = function(e) {
+            if (e.target.classList.contains("cell")) {
+                if (e.target.textContent == "") {
+                    e.target.textContent = activePlayer.marker;
+                    currentRow = Number(e.target.classList[1].at(-1)) - 1;
+                    currentColumn = Number(e.target.classList[2].at(-1)) - 1;
+                    pubsub.emit("editGameBoard", activePlayer, currentRow, currentColumn);
+                } else {alert("This spot is taken!")};
             };
         };
-    }; 
-
-    const switchPlayer = function(e) {
-        if (e.target.classList.contains("cell") && valid) {
-            (activePlayer == players[0]) ? activePlayer = players[1] : activePlayer = players[0];
+        const enablePlaceMarker = function() {
+                document.body.addEventListener("click", placeMarker, false);
         };
-    };
+        const switchPlayer = function(e) {
+            if (e.target.classList.contains("cell") && e.target.textContent == activePlayer.marker) {
+                (activePlayer == players[0]) ? activePlayer = players[1] : activePlayer = players[0];
+            };
+        };
+        const enableSwitchPlayer = function() {
+            document.body.addEventListener("click", switchPlayer, false);
+        };
 
-    function checkWin() {
+        const stopRoundIfWin = function() {
+            if (checkWin(gameBoard.getGameBoard())) {
+                pubsub.emit("openGameOver", (activePlayer == players[0]) ? players[1].name : players[0].name);
+                document.body.removeEventListener("click", placeMarker, false);
+                document.body.removeEventListener("click", switchPlayer, false);
+            };
+        };
+
+        enablePlaceMarker();
+        enableSwitchPlayer();
+
+        document.body.addEventListener("click", stopRoundIfWin, false);
+    };
+    
+    const checkWin = function(gameBoard) {
         let win = false;
-        for (let i=0; i<gameBoard.getGameBoard().rows; i++) {
+        for (let i=0; i<gameBoard.rows; i++) {
             if (!win) {
-                win = gameBoard.getGameBoard().board[i].every((element) => ((element === gameBoard.getGameBoard().board[i][0]) && (gameBoard.getGameBoard().board[i][0] == "X" || gameBoard.getGameBoard().board[i][0] == "O")));
+                win = gameBoard.board[i].every((element) => ((element === gameBoard.board[i][0]) && (gameBoard.board[i][0] == "X" || gameBoard.board[i][0] == "O")));
             };
         };
         if (!win) {
-            for (let i=0; i<gameBoard.getGameBoard().columns; i++) {
+            for (let i=0; i<gameBoard.columns; i++) {
                 if (!win) {
-                    win = gameBoard.getGameBoard().board.every((element) => ((element[i] === gameBoard.getGameBoard().board[0][i]) && (gameBoard.getGameBoard().board[0][i] == "X" || gameBoard.getGameBoard().board[0][i] == "O")));
+                    win = gameBoard.board.every((element) => ((element[i] === gameBoard.board[0][i]) && (gameBoard.board[0][i] == "X" || gameBoard.board[0][i] == "O")));
                 };
             };
         };
-        if (!win) {win = gameBoard.getGameBoard().board.every((element) => (element[gameBoard.getGameBoard().board.indexOf(element)] === gameBoard.getGameBoard().board[1][1]) && (gameBoard.getGameBoard().board[1][1] == "X" || gameBoard.getGameBoard().board[1][1] == "O"));};
-        if (!win) {win = gameBoard.getGameBoard().board.every((element) => (element[gameBoard.getGameBoard().rows - 1 - gameBoard.getGameBoard().board.indexOf(element)] === gameBoard.getGameBoard().board[1][1])  && (gameBoard.getGameBoard().board[1][1] == "X" || gameBoard.getGameBoard().board[1][1] == "O"));};
+        if (!win) {win = gameBoard.board.every((element) => (element[gameBoard.board.indexOf(element)] === gameBoard.board[1][1]) && (gameBoard.board[1][1] == "X" || gameBoard.board[1][1] == "O"));};
+        if (!win) {win = gameBoard.board.every((element) => (element[gameBoard.rows - 1 - gameBoard.board.indexOf(element)] === gameBoard.board[1][1])  && (gameBoard.board[1][1] == "X" || gameBoard.board[1][1] == "O"));};
         return win;
     };
 
-    const enablePlaceMarker = function() {
-        document.body.addEventListener("click", placeMarker, false);
+    const eraseMarker = function() {
+        let board = document.querySelectorAll(".cell");
+        Array.from(board).forEach(element => element.textContent = "");
     };
 
-    const enableSwitchPlayer = function() {
-        document.body.addEventListener("click", switchPlayer, false);
+    const restartGame = function() {
+        pubsub.emit("resetGameBoard");
+        eraseMarker();
+        startGame();
     };
 
-    const disablePlaceMarker = function() {
-        document.body.removeEventListener("click", placeMarker, false);
-    };
+    pubsub.on("restartGame", restartGame);
 
-    const disableSwitchPlayer = function() {
-        document.body.removeEventListener("click", switchPlayer, false);
-    };
-
-    const enableCheckWin = function() {
-        document.body.addEventListener("click", checkWin, false)
-    };
-
-    initRound();
+    startGame();
 })();
